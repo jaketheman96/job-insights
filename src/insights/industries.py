06d007532acb2f73939052813dd9a5af4134f66a
Jake Chien <jake_chien@outlook.com>
2023-01-25 15:13:36 -0300

    feat: get unique industries implemented

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    industries = []
+    file = read(path)
+    for industry in file:
+        if industry["industry"] not in industries:
+            industries.append(industry["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
