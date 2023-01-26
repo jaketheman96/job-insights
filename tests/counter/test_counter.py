77562bf86852e4d862ba0c0289057d704754dc11
Jake Chien <jake_chien@outlook.com>
2023-01-26 19:46:55 -0300

    test: testing count ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    word_1 = "Python"
+    word_2 = "Javascript"
+    count_1 = count_ocurrences(path, word_1)
+    count_2 = count_ocurrences(path, word_2)
+    assert count_1 == 1639
+    assert count_2 == 122
